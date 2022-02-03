a3c768385f266a307e600b5b2ab032605b749006
Wajih <wajihhass1@gmail.com>
2022-02-03 13:51:04 -0500

    Milestone Project1

---
+++ MainPage/Computer/Computer-Categories/GeneralQuizzes/Cosyname1quiz/q.js
@@ -0,0 +1,6 @@
+
+function StartGame(qnr, score) {
+	
+	document.getElementById('content').innerHTML = '<div id="qscreen" style="opacity: 1;"><div id="qscreenimg" style="font-size: 1.7em; line-height: normal;">Loading</div></div>';
+	
+}	
